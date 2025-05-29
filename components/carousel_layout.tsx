01d71fb034c4fb3d8834caa70d52b6c2ffb0127d
Ricky <ricardojsribeiro99@gmail.com>
2025-05-29 21:55:02 +0100

    Carousel Images responsiveness

--- components/carousel_layout.tsx
+++ components/carousel_layout.tsx
@@ -6,7 +6,12 @@ export default function Carousel_Layout({ images, direction }: { images: any[]; 
         <div className="relative">
             <Marquee autoFill speed={50} style={{}} direction={direction}>
                 {images?.map((image: any, i: any) => (
-                    <img key={i} src={image.image} alt={`Image ${i + 1}`} className="w-full h-[400px] object-cover" />
+                    <img
+                        key={i}
+                        src={image.image}
+                        alt={`Image ${i + 1}`}
+                        className="lg:w-full lg:h-[400px] sm:h-[200px] sm:w-[300px] h-[150px] w-[150px] object-cover"
+                    />
                 ))}
             </Marquee>
         </div>
